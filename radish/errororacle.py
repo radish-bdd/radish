0ef90d52daddac975f40a1172b14ba336668bce0
Timo Furrer <tuxtimo@gmail.com>
2015-06-16 22:02:47 +0200

    errororacle: handle KeyboardInterrupt

--- radish/errororacle.py
+++ radish/errororacle.py
@@ -12,18 +12,25 @@ from colorful import colorful
 from radish.exceptions import RadishError, HookError
 
 
+def write(text):
+    """
+        Writes the given text to the console
+    """
+    print(text)
+
+
 def write_error(text):
     """
         Writes the given text to the console
     """
-    print("{}: {}".format(colorful.bold_red("Error"), colorful.red(text)))
+    write("{}: {}".format(colorful.bold_red("Error"), colorful.red(text)))
 
 
 def write_failure(failure):
     """
         Writes the failure to the console
     """
-    print("\n{}".format(colorful.red(failure.traceback)))
+    write("\n{}".format(colorful.red(failure.traceback)))
 
 
 def abort(return_code):
@@ -50,6 +57,9 @@ def error_oracle(func):
             abort(1)
         except RadishError as e:
             write_error(e)
+            abort(1)
+        except KeyboardInterrupt:
+            write("Aborted by the user..")
             abort(1)
         except Exception as e:  # pylint: disable=broad-except
             write_error(e)

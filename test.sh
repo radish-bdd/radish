76b8934e28bfa3a0e1d194981bd56a1719eb5816
Timo Furrer <tuxtimo@gmail.com>
2015-11-07 17:30:55 -0800

    Remove test.sh file

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-NOSEBIN="env/bin/nosetests"
-if [ ! -f "$NOSEBIN" ]; then
-    NOSEBIN=nosetests
-fi
-
-$NOSEBIN -v tests/unit/ --rednose --with-cover --cover-package=radish/ --cover-erase

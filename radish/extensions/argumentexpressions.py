3e8ba5a2c2416bb020e08a2fe2b73b81b88d823a
Timo Furrer <tuxtimo@gmail.com>
2015-11-15 20:25:16 -0800

    Remove obsolete code

--- radish/extensions/argumentexpressions.py
+++ radish/extensions/argumentexpressions.py
@@ -7,32 +7,6 @@
 from radish.argexpregistry import arg_expr
 
 
-@arg_expr("Number", r"(\+|-)?\d+")
-def arg_expr_number(text):
-    """
-        Argument Expression which expects a valid number
-
-        :param str text: the text which was matched as number
-
-        :returns: integer number
-        :rtype: int
-    """
-    return int(text)
-
-
-@arg_expr("FloatNumber", r"(\+|-)?\d+(\.\d+)?")
-def arg_expr_floatnumber(text):
-    """
-        Argument Expression which expects a valid floating point number
-
-        :param str text: the text which was matched as floating point number
-
-        :returns: float number
-        :rtype: float
-    """
-    return float(text)
-
-
 @arg_expr("MathExpression", r"[0-9 +\-*/%.e]+")
 def arg_expr_mathexpression(text):
     """
@@ -44,16 +18,3 @@ def arg_expr_mathexpression(text):
         :rtype: float
     """
     return float(eval(text))
-
-
-@arg_expr("VariableName", r"[A-Za-z_][A-Za-z0-9_]*")
-def arg_expr_variablename(text):
-    """
-        Argument Expression which expects a variable name
-
-        :param str text: the text which was matched as variable name
-
-        :returns: the variable name
-        :rtype: str
-    """
-    return text

d116ac9368b857d1a946c3e0c6d8cc44e4cc79be
Timo Furrer <tuxtimo@gmail.com>
2015-11-20 14:08:20 -0800

    Add logo to the documentation

--- docs/conf.py
+++ docs/conf.py
@@ -121,7 +121,14 @@ html_theme = 'alabaster'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    "logo": "radish-bdd-logo.png",
+    "logo_name": "radish",
+    "github_user": "radish-bdd",
+    "github_repo": "radish",
+    "github_button": True,
+    "github_banner": True
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
@@ -135,12 +142,12 @@ html_theme = 'alabaster'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = "_static/radish-bdd-logo-trans-bg.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = "_static/radish-bdd-logo.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,

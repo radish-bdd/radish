69380c171846a67994183fc14dda0508eb7cd44c
Timo Furrer <tuxtimo@gmail.com>
2016-09-25 20:36:05 +0200

    Fix shellcheck issues

--- tests/functional.sh
+++ tests/functional.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-ROOT=`dirname $0`
+ROOT=$(dirname "$0")
 TESTS_ROOT=functional
 RADISH_BASEDIR=radish
 FEATURES_DIR=features
 
-cd $ROOT/$TESTS_ROOT
+cd "$ROOT/$TESTS_ROOT"
 
-for t in `ls`; do
-    radish -b $t/$RADISH_BASEDIR $t/$FEATURES_DIR
+for t in *; do
+    radish -b "$t/$RADISH_BASEDIR" "$t/$FEATURES_DIR"
     if [ $? -ne 0 ]; then
         echo "Functional tests from '$t' failed with status $?"
         exit 1

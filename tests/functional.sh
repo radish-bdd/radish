2f2f4abd0826952f1a8f879a5bf5fc8b9f434e6e
Timo Furrer <tuxtimo@gmail.com>
2017-02-11 16:20:04 +0100

    Fix coverage for tox

--- tests/functional.sh
+++ tests/functional.sh
@@ -5,10 +5,15 @@ TESTS_ROOT=functional
 RADISH_BASEDIR=radish
 FEATURES_DIR=features
 
+#export COVERAGE_FILE=$(pwd)/.coverage
+BASE_COVERAGE_FILE=$(pwd)/.coverage
+COVERAGE_RC=$(pwd)/.coveragerc
+
 cd "$ROOT/$TESTS_ROOT"
 
 for t in *; do
-    radish -b "$t/$RADISH_BASEDIR" "$t/$FEATURES_DIR"
+    export COVERAGE_FILE=${BASE_COVERAGE_FILE}.$t
+    coverage run -a --rcfile=$COVERAGE_RC --source=radish ${1}/radish -b "$t/$RADISH_BASEDIR" "$t/$FEATURES_DIR"
     if [ $? -ne 0 ]; then
         echo "Functional tests from '$t' failed with status $?"
         exit 1

e10f70d778bae7bab8338f2cd8bf2f2b1e532ea7
Samuel Yvon <SamuelYvon@users.noreply.github.com>
2017-03-21 13:30:18 -0400

    Fix for issue #53 (#54)
    Use longest matching step pattern if multiple would match. Closes #53 
    
    * Keep longuest matches only in case many matches are made
    
    * Revert "Keep longuest matches only in case many matches are made"
    
    This reverts commit 329b2455ba0ed9c8b22f442d72fe9c7921b08934.
    
    * Only take matches that fill the whole sentence
    
    * Added a test case for steps with similar names
    
    * Added two test to cover non-regex matching with similar sentences
    
    * Using the subscript construct to get a range of element instead of going index based

--- tests/unit/test_matcher.py
+++ tests/unit/test_matcher.py
@@ -15,6 +15,7 @@ class MatcherTestCase(RadishTestCase):
     """
         Tests the Matcher class
     """
+
     @classmethod
     def setUpClass(cls):
         """
@@ -28,7 +29,8 @@ class MatcherTestCase(RadishTestCase):
         """
             Test matching steps from feature files with registered steps
         """
-        steps = {re.compile(r"Given I have the number (\d+)"): "some_func", re.compile(r"I add (\d+) to my number"): "some_other_func"}
+        steps = {re.compile(r"Given I have the number (\d+)"): "some_func",
+                 re.compile(r"I add (\d+) to my number"): "some_other_func"}
 
         match, func = match_step("Given I have the number 5", steps)
         arguments, keyword_arguments = match.evaluate()
@@ -45,11 +47,74 @@ class MatcherTestCase(RadishTestCase):
         match = match_step("when I call a non-existing step", steps)
         match.should.be.none  # pylint: disable=pointless-statement
 
+    def test_similar_steps_pattern(self):
+        """
+            Test matching steps from feature files with similar patterns
+        """
+        steps = {re.compile(r"Given I have the number (\d+)"): "some_func",
+                 re.compile(r"Given I have the number (\d+) to add"): "some_other_func"}
+
+        match, func = match_step("Given I have the number 5", steps)
+        arguments, keyword_arguments = match.evaluate()
+        arguments.should.be.equal(("5",))
+        keyword_arguments.should.be.equal({})
+        func.should.be.equal("some_func")
+
+        match, func = match_step("Given I have the number 5 to add", steps)
+        arguments, keyword_arguments = match.evaluate()
+        arguments.should.be.equal(("5",))
+        keyword_arguments.should.be.equal({})
+        func.should.be.equal("some_other_func")
+
+        match = match_step("Given I have the number", steps)
+        match.should.be.none  # pylint: disable=pointless-statement
+
+    def test_similar_steps(self):
+        """
+            Test matching steps from feature files with similar text
+        """
+        steps = {"This is a short sentence": "some_func",
+                 "This is a short sentence with few more words": "some_other_func"}
+
+        match, func = match_step("This is a short sentence", steps)
+        arguments, keyword_arguments = match.evaluate()
+        keyword_arguments.should.be.equal({})
+        func.should.be.equal("some_func")
+
+        match, func = match_step("This is a short sentence with few more words", steps)
+        arguments, keyword_arguments = match.evaluate()
+        keyword_arguments.should.be.equal({})
+        func.should.be.equal("some_other_func")
+
+        match = match_step("This is not a step", steps)
+        match.should.be.none  # pylint: disable=pointless-statement
+
+    def test_similar_steps_with_args(self):
+        """
+            Test matching steps from feature files with similar text and arguments
+        """
+        steps = {"This is a short {arg:S}": "some_func",
+                 "This is a short {arg:S} with few more words": "some_other_func"}
+
+        match, func = match_step("This is a short sentence", steps)
+        arguments, keyword_arguments = match.evaluate()
+        assert keyword_arguments["arg"] == "sentence"
+        func.should.be.equal("some_func")
+
+        match, func = match_step("This is a short sentence with few more words", steps)
+        arguments, keyword_arguments = match.evaluate()
+        assert keyword_arguments["arg"] == "sentence"
+        func.should.be.equal("some_other_func")
+
+        match = match_step("This is not a step", steps)
+        match.should.be.none  # pylint: disable=pointless-statement
+
     def test_merge_steps(self):
         """
             Test merging steps from feature files with registered steps
         """
-        steps = {re.compile(r"Given I have the number (\d+)"): "some_func", re.compile(r"I add (\d+) to my number"): "some_other_func"}
+        steps = {re.compile(r"Given I have the number (\d+)"): "some_func",
+                 re.compile(r"I add (\d+) to my number"): "some_other_func"}
 
         feature = Feature(1, "Feature", "Some feature", "test.feature", 1)
         scenario = Scenario(1, "Scenario", "Adding numbers", "test.feature", 2, feature)
@@ -68,7 +133,8 @@ class MatcherTestCase(RadishTestCase):
         """
             Test merging non existing step
         """
-        steps = {re.compile(r"Given I have the number (\d+)"): "some_func", re.compile(r"I add (\d+) to my number"): "some_other_func"}
+        steps = {re.compile(r"Given I have the number (\d+)"): "some_func",
+                 re.compile(r"I add (\d+) to my number"): "some_other_func"}
 
         feature = Feature(1, "Feature", "Some feature", "test.feature", 1)
         scenario = Scenario(1, "Scenario", "Adding numbers", "test.feature", 2, feature)

1fc210a1a74e73b21773ea2fc88c118190224b45
fliiiix <hi@l33t.name>
2019-03-16 21:49:21 +0100

    Cleanup exploratory unicode test and add more 🎉
    This is possible now thanks to PyYaml 5.1.

--- tests/exploratory/unicode/radish/steps.py
+++ tests/exploratory/unicode/radish/steps.py
@@ -2,19 +2,16 @@
 
 from __future__ import unicode_literals
 
-import re
-
 from radish.stepregistry import step
-from radish import given, when, then
+from radish import when, then
 
 
-# @step(re.compile(r"I have the number (\d+)"))
 @step("I have the number {number:g}")
 def have_number(step, number):
     step.context.numbers.append(int(number))
 
 
-@when("I sum them µŴ")
+@when("I sum them 🎉")
 def sum_numbers(step):
     step.context.result = sum(step.context.numbers)
 

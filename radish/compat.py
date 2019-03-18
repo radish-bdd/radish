40360968e11ed8c553e078b8421f31cc5e82531f
Timo Furrer <tuxtimo@gmail.com>
2019-03-18 18:49:24 +0000

    Remove Python > 3.5 RecursionError compatibility

--- radish/compat.py
+++ radish/compat.py
@@ -2,14 +2,9 @@
 This module provide Python 2 / Python 3 compatability
 """
 
-import sys
 import re
 
 try:
     re_pattern = re.Pattern  # >= Python 3.7
 except AttributeError:
     re_pattern = re._pattern_type
-
-
-# RecursionError does not exist in Python < 3.5
-RecursionError = RuntimeError if sys.version_info < (3, 5) else RecursionError

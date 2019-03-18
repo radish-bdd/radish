253ad1aa4d5f311134f7f959f9822569199ceb35
Timo Furrer <tuxtimo@gmail.com>
2019-03-18 18:39:23 +0000

    Remove checks for Python 2 in arbitrary modules

--- radish/extensions/formatters/gherkin.py
+++ radish/extensions/formatters/gherkin.py
@@ -17,11 +17,6 @@ from radish.scenarioloop import ScenarioLoop
 from radish.stepmodel import Step
 from radish.extensionregistry import extension
 from radish.utils import console_write as write
-from radish.compat import PY2
-
-
-if PY2:
-    str = unicode
 
 
 @extension

941f2b6fd801b69ad3251584e3a18df8ea216d74
Timo Furrer <tuxtimo@gmail.com>
2019-11-09 12:16:45 +0100

    Test top-level package exports

---
+++ tests/integration/test_exports.py
@@ -0,0 +1,52 @@
+"""
+radish
+~~~~~~
+
+The root from red to green. BDD tooling for Python.
+
+:copyright: (c) 2019 by Timo Furrer <tuxtimo@gmail.com>
+:license: MIT, see LICENSE for more details.
+"""
+
+
+def test_correct_exports_on_package_level():
+    # THEN, can import top-level exports
+    from radish import (  # noqa
+        # Step Decorators
+        given,
+        when,
+        then,
+        step,
+
+        # Hooks & Terrain
+        world,
+        before,
+        after,
+
+        # Step Pattern Types
+        custom_type,
+        register_custom_type,
+        TypeBuilder,
+
+        # The radish parser
+        FeatureFileParser,
+
+        # Models
+        Feature,
+        Rule,
+        DefaultRule,
+        Background,
+        Scenario,
+        ScenarioOutline,
+        ScenarioLoop,
+        Step,
+        Tag,
+        PreconditionTag,
+        ConstantTag,
+
+        # Exception Types
+        RadishError,
+
+        # The radish-bdd package version
+        __version__
+    )

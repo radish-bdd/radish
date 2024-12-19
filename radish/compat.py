13584f9ffb0db86ab5d6a5f9ae311630056a66fb
fliiiix <hi@l33t.name>
2024-12-19 10:22:06 +0100

    RecursionError now exist in all supported versions

--- radish/compat.py
+++
@@ -1,8 +0,0 @@
-"""
-This module provide Python 3 compatability
-"""
-
-import sys
-
-# RecursionError does not exist in Python < 3.5
-RecursionError = RuntimeError if sys.version_info < (3, 5) else RecursionError

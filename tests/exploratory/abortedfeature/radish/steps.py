becca9c12dbf9dd0e8acaf1e217765be7675a608
fliiiix <hi@l33t.name>
2025-11-01 08:40:30 +0100

    Add exploratory test feature aborted
    Run with: radish -b radish features --junit-xml=result.xml
    
    Produces junit xml:
    ```
    <?xml version='1.0' encoding='utf-8'?>
    <testsuites time="0.000">
      <testsuite name="Test summing numbers" failures="0" errors="0" skipped="1" tests="2" time="0.000">
        <testcase classname="Test summing numbers" name="Sum two numbers" time="0.000"/>
        <testcase classname="Test summing numbers" name="Sum three numbers" time="0.000">
          <skipped/>
        </testcase>
      </testsuite>
      <testsuite name="Test summing numbers" failures="0" errors="0" skipped="2" tests="2" time="0.000">
        <testcase classname="Test summing numbers" name="Sum two numbers" time="0.000">
          <skipped/>
        </testcase>
        <testcase classname="Test summing numbers" name="Sum three numbers" time="0.000">
          <skipped/>
        </testcase>
      </testsuite>
    </testsuites>
    ```

---
+++ tests/exploratory/abortedfeature/radish/steps.py
@@ -0,0 +1,24 @@
+import sys
+
+from radish import then, when
+from radish.stepregistry import step
+
+
+@step("I have the number {number:g}")
+def have_number(step, number):
+    step.context.numbers.append(int(number))
+
+
+@when("I sum them")
+def sum_numbers(step):
+    step.context.result = sum(step.context.numbers)
+
+
+@then("I expect the result to be {result:g}")
+def expect_result(step, result):
+    assert step.context.result == result
+
+
+@then("I crash")
+def crash(step):
+    sys.exit(1)
